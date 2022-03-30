f65b83b09ef6e5196c7b34b40b99093d00a7931c
Mohab <khalifamohab97@outlook.com>
2022-03-30 20:23:57 +0100

    some lines in runner

--- src/main/java/com/qa/ims/Runner.java
+++ src/main/java/com/qa/ims/Runner.java
@@ -3,14 +3,23 @@ package com.qa.ims;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import com.qa.ims.persistence.dao.CustomerDAO;
+import com.qa.ims.persistence.domain.Customer;
+
 public class Runner {
 
 	public static final Logger LOGGER = LogManager.getLogger();
 
 	public static void main(String[] args) {
-		IMS ims = new IMS();
-		ims.imsSystem();
-		LOGGER.info("SO LONG!");
+//		IMS ims = new IMS();
+//		ims.imsSystem();
+//		LOGGER.info("SO LONG!");
+
+		CustomerDAO customerDAO = new CustomerDAO();
+
+		Customer customer = customerDAO.read(1);
+		System.out.println(customer);
+
 	}
 
 }
