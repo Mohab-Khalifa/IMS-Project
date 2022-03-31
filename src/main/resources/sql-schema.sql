59a2059751fd2c5f3a1903989adffad534912d91
Mohab <khalifamohab97@outlook.com>
2022-03-31 17:15:36 +0100

    updated data.sql and schema.sql files

--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -1,18 +1,18 @@
-drop schema ims;
+DROP SCHEMA ims;
 
 CREATE SCHEMA IF NOT EXISTS `ims`;
 
 USE `ims` ;
 
 CREATE TABLE IF NOT EXISTS `ims`.`customers` (
-    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `id` BIGINT AUTO_INCREMENT,
     `first_name` VARCHAR(40) NOT NULL,
     `surname` VARCHAR(40) NOT NULL,
     PRIMARY KEY (`id`)
 );
 
 CREATE TABLE IF NOT EXISTS `ims`.`items` (
-	`id` INT(11) NOT NULL AUTO_INCREMENT,
+	`id` BIGINT AUTO_INCREMENT,
 	`item_name` VARCHAR(40) NOT NULL,
 	`item_category` VARCHAR(40) NOT NULL,
 	`price` DECIMAL(6,2) NOT NULL,
@@ -20,7 +20,7 @@ CREATE TABLE IF NOT EXISTS `ims`.`items` (
 );
 
 CREATE TABLE IF NOT EXISTS `ims`.`orders` (
-	`id` INT(11) NOT NULL AUTO_INCREMENT,
+	`id` BIGINT AUTO_INCREMENT,
 	`fk_customers_id` INT NOT NULL,
 	`total_price` DECIMAL(6,2) NOT NULL,
 	`date_placed` DATE,
@@ -31,6 +31,8 @@ CREATE TABLE IF NOT EXISTS `ims`.`orders` (
 CREATE TABLE IF NOT EXISTS `ims`.`orders_items` (
 	`fk_orders_id` INT NOT NULL,
 	`fk_items_id` INT NOT NULL,
+	`quantity` INT DEFAULT 1,
+	PRIMARY KEY (`fk_orders_id`, `fk_items_id`),
 	FOREIGN KEY (`fk_orders_id`) REFERENCES orders(`id`),
 	FOREIGN KEY (`fk_items_id`) REFERENCES items(`id`)
 );
