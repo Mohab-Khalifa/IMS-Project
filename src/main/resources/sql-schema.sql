73813c3d4fd2144fd5b24f085eaa5c7f39487b61
Mohab <khalifamohab97@outlook.com>
2022-03-31 12:28:05 +0100

    updated schema.sql

--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -18,3 +18,19 @@ CREATE TABLE IF NOT EXISTS `ims`.`items` (
 	`price` DECIMAL(6,2) NOT NULL,
 	PRIMARY KEY (`id`)
 );
+
+CREATE TABLE IF NOT EXISTS `ims`.`orders` (
+	`id` INT(11) NOT NULL AUTO_INCREMENT,
+	`fk_customers_id` INT NOT NULL,
+	`total_price` DECIMAL(6,2) NOT NULL,
+	`date_placed` DATE,
+	PRIMARY KEY (`id`),
+	FOREIGN KEY (`fk_customers_id`) REFERENCES customers(`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `ims`.`orders_items` (
+	`fk_orders_id` INT NOT NULL,
+	`fk_items_id` INT NOT NULL,
+	FOREIGN KEY (`fk_orders_id`) REFERENCES orders(`id`),
+	FOREIGN KEY (`fk_items_id`) REFERENCES items(`id`)
+);
