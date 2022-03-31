223c0ed8ad6dba8254617bf0112820291b6ca02d
Mohab <khalifamohab97@outlook.com>
2022-03-31 12:31:25 +0100

    updated data.sql file

--- src/main/resources/sql-data.sql
+++ src/main/resources/sql-data.sql
@@ -1,2 +1,3 @@
 INSERT INTO `ims`.`customers` (`first_name`, `surname`) VALUES ('Mohab', 'Khalifa');
 INSERT INTO `ims`.`items` (`item_name`, `item_category`, `price`) VALUES ('PS5', 'Gaming', '449.99');
+INSERT INTO `ims`.`orders` (`id`, `customers_id`) VALUES (1L, 1L);
