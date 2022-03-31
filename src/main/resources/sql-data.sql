19be6b3f48a1b02f38c14146d57cb9a888fe80a6
Mohab <khalifamohab97@outlook.com>
2022-03-31 10:37:49 +0100

    updated sql data and schema

--- src/main/resources/sql-data.sql
+++ src/main/resources/sql-data.sql
@@ -1,1 +1,2 @@
-INSERT INTO `ims`.`customers` (`first_name`, `surname`) VALUES ('jordan', 'harrison');
+INSERT INTO `ims`.`customers` (`first_name`, `surname`) VALUES ('Mohab', 'Khalifa');
+INSERT INTO `ims`.`items` (`item_name`, `item_category`, `price`) VALUES ('PS5', 'Gaming', '449.99');
--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -15,6 +15,6 @@ CREATE TABLE IF NOT EXISTS `ims`.`items` (
 	`id` INT(11) NOT NULL AUTO_INCREMENT,
 	`item_name` VARCHAR(40) NOT NULL,
 	`item_category` VARCHAR(40) NOT NULL,
-	`price` DECIMAL(4,2) NOT NULL
+	`price` DECIMAL(6,2) NOT NULL,
 	PRIMARY KEY (`id`)
 );
