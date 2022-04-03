886ae79ba401d20efdd145ed9e3052817780b7e9
Mohab <khalifamohab97@outlook.com>
2022-04-03 12:18:23 +0100

    updated test/main sql-schemas and main sql-data

--- src/main/resources/sql-data.sql
+++ src/main/resources/sql-data.sql
@@ -1,3 +1,3 @@
 INSERT INTO `customers` (`first_name`, `surname`) VALUES ('Mohab', 'Khalifa');
 INSERT INTO `items` (`item_name`, `item_category`, `price`) VALUES ('PS5', 'Gaming', '449.99');
-INSERT INTO `orders` (`id`, `fk_customers_id`, `total_price`) VALUES (1L, 1L, 9.99D);
+INSERT INTO `orders` (`id`, `fk_customers_id`) VALUES (1L, 1L);
